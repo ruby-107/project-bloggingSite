efcfa510378b8677f3f05be04751695cf5db0212
ruby-107 <99881236+ruby-107@users.noreply.github.com>
2022-03-21 14:59:21 +0530

    Update authorModel.js

--- src/model/authorModel.js
+++ src/model/authorModel.js
@@ -15,19 +15,20 @@ const authorSchema = new mongoose.Schema({
         type:String,
         enum: ['Mr', 'Mrs', 'Miss']
     },
-    email: {
-        unique:true,
-         type: String,
-         required: true,
-         match: [/^\w+([\.-]?\w+)@\w+([\. -]?\w+)(\.\w{2,3})+$/, 'Please fill a valid email address']
-     },
 
+    email: {
+        type:String,
+        unique: true,
+         validate:{
+             validator: validator.isEmail,
+             message: '{VALUE} is not a valid email',
+             isAsync: false
+           },
     password: {
         type: String,
         requried: true,
-        match:[/^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[0-9a-zA-Z]).{8,}$/,"Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long"]
-
-    
+       
+}
 }
 
 }, { timestamps: true })
